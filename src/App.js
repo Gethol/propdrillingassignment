0bee96de48174e480a9a1f88b120bf77a679d8e4
Gethol <natenderitu@gmail.com>
2024-03-25 14:39:58 +0200

    chore: initialise repository with create react app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import { PropDrilling } from './components/PropDrilling.tsx';
+
+
+
+function App() {
+  return (
+    <PropDrilling level = {1} />
+   
+  );
+}
+
+export default App;
