a4b0e6767e902274f4cc7ffa0a3f300064aef6d0
Gethol <natenderitu@gmail.com>
2024-03-25 15:35:04 +0200

    fix: output the contextProvider output

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import { PropDrilling } from './components/PropDrilling.tsx';
+import {ContextProvider} from './context/ContextProvider.tsx'
 
 
 
 function App() {
   return (
-    <PropDrilling level = {1} />
-   
+    // <PropDrilling level = {1} />
+    <ContextProvider level = {4} />
+    
   );
 }
 
