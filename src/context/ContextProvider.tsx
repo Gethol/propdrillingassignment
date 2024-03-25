1f98f16bdd013e0d90f5071984fac2c04d6f9199
Gethol <natenderitu@gmail.com>
2024-03-25 15:32:14 +0200

    feat: set up levelContext provider

---
+++ src/context/ContextProvider.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+import { useContext, createContext, useState } from "react";
+import { LevelContext } from "./LevelContext.tsx";
+import Section from "./Section.tsx";
+
+const ContextProvider = () => {
+    const [level, setLevel] = useState(0)
+
+    return(
+    <LevelContext.Provider value = {{level}}>
+        <Section />
+    </LevelContext.Provider>
+    );
+}
