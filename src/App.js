1163257031f05652a8b603c0414f762ea71c98d3
Gethol <natenderitu@gmail.com>
2024-03-25 16:40:13 +0200

    feat: add login and sign up components to App.js

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 import { PropDrilling } from './components/PropDrilling.tsx';
 import {ContextProvider} from './context/ContextProvider.tsx'
+import Login from './components/forms/Login.tsx';
+import Signup from './components/forms/Signup.tsx';
 
 
 
 function App() {
   return (
-    // <PropDrilling level = {1} />
-    <ContextProvider />
-    
+    <>
+    {/* <PropDrilling level = {1} />
+    <ContextProvider /> */}
+    <Login />
+    <Signup />
+    </>
   );
 }
 
